61e27c59385e9487283faee0e3cd33508f5f8a3f
JiangChencheng <chencheng.jiang@v.show>
2024-05-10 11:08:58 +0800

    添加一些参数

--- lib/pod/command/kratos.rb
+++ lib/pod/command/kratos.rb
@@ -9,42 +9,80 @@ module Pod
       DESC
 
       self.arguments = [
-        CLAide::Argument.new('NAME', true),
-        CLAide::Argument.new('SOURCE', false)
+        CLAide::Argument.new('NAME', true)
       ]
 
       def self.options
         [
+          %w[--archs 生成的架构.默认为：`arm64`],
+          %w[--local 使用本地版本构建.],
           %w[--mixup 开启构建时代码混淆功能.],
-          ['--force',     'Overwrite existing files.']
+          %w[--old-class-prefix 混淆时修改的类前缀.默认为：`BT`],
+          %w[--new-class-prefixes 混淆时要修改的目标类前缀，多个用,隔开.默认为：`MNL`],
+          %w[--filter-file-prefixes 混淆时要忽略的文件前缀，多个用,隔开.默认为：`Target_`],
+          %w[--no-clean 构建失败不清除构建工程.],
+          %w[--clean-cache 构建时清除本地所有的组件缓存.⚠️注意：开启后会重新下载所有组件],
+          ['--embedded',  'Generate embedded frameworks.'],
+          ['--library',   'Generate static libraries.'],
+          ['--dynamic',   'Generate dynamic framework.'],
+          %w[--upgrade-swift 升级Swift版本]
         ]
       end
 
       def initialize(argv)
         @name = argv.shift_argument
-        @source = argv.shift_argument
-        @force = argv.flag?('force')
         @spec = spec_with_path(@name)
-        # @is_spec_from_path = true if @spec
+        @is_spec_from_path = true if @spec
         @spec ||= spec_with_name(@name)
+
+
+        @local = argv.flag?('local', false)
+        @archs = argv.option('archs', 'arm64').split(',')
+        @clean_sandbox = argv.flag?('clean', true)
+
+        @embedded = argv.flag?('embedded')
+        @library = argv.flag?('library')
+        @dynamic = argv.flag?('dynamic')
+
+        @package_type = if @embedded
+                          :static_framework
+                        elsif @dynamic
+                          :dynamic_framework
+                        elsif @library
+                          :static_library
+                        else
+                          :static_framework
+                        end
+
         # 代码混淆配置项
         @mixup = argv.flag?('mixup', false)
         @old_class_prefix = argv.option('old-class-prefix', 'BT')
         @new_class_prefixes = argv.option('new-class-prefixes', 'MNL').split(',')
         @filter_file_prefixes = argv.option('filter-file-prefixes', 'Target_,').split(',')
 
+        # 更新本地缓存
+        @clean_cache = argv.flag?('clean-cache', false)
+
+        # 升级Swift版本
+        @upgrade_swift_packager = argv.flag?('upgrade-swift', false)
+
+        @swift_version = local_swift_version
+
         super
+      end
+
+      def local_swift_version
+        _, stdout, _ = Open3.popen3('xcrun swift --version')
+        stdout.gets.to_s.gsub(/version (\d+(\.\d+)+)/).to_a[0].split(' ')[1]
       end
 
       def validate!
         super
         help! 'A podspec name or path is required.' unless @spec
+        help! '--local option can only be used when a local `.podspec` path is given.' if @local && !@is_spec_from_path
       end
 
       def run
-
-        UI.puts "The name of the Pod is #{@name}"
-        UI.puts "The spec of the Pod is #{@spec}".red
 
         build_with_mixup if @mixup
 
